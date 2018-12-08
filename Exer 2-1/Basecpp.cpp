1688d2dc028eaf4b861f8242b08fbfc17cd763c2
Michael Reyes <michaelreyes808@gmail.com>
2018-12-08 16:41:55 -0700

    This is a test for using the GitHub System

---
+++ Exer 2-1/Basecpp.cpp
@@ -0,0 +1,202 @@
+/*
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+void Exer_2_1();
+void Exer_2_2();
+void Exer_2_3();
+void Exer_2_4();
+void Exer_2_5();
+void Exer_2_6();
+void Exer_2_7();
+void Exer_2_8();
+void Exer_2_9();
+void Exer_2_10();
+void Exer_2_11();
+void Exer_2_12();
+void Exer_2_13();
+void Exer_2_14();
+void Exer_2_15();
+void Exer_2_16();
+void Exer_2_17();
+void Exer_2_18();
+void Exer_2_19();
+void Exer_2_20();
+void Exer_2_21();
+void Exer_2_22();
+void Exer_2_23();
+void Exer_2_24();
+void Exer_2_25();
+void Exer_2_26();
+void Exer_2_27();
+
+
+
+
+
+
+
+
+// pch.cpp: source file corresponding to pre-compiled header; necessary for compilation to succeed
+
+#include "pch.h"
+#include <iostream>
+
+// In general, ignore this file, but keep it around if you are using pre-compiled headers.
+
+void ExerTest_2_1()
+{
+	// Completed Exercise 1
+	std::cout << "**********************************\n";
+	std::cout << "*     Programming Assignment 1   *\n";
+	std::cout << "*      Computer Programming I    *\n";
+	std::cout << "*        Author: Michael Reyes   *\n";
+	std::cout << "*   Due Date: Saturday, Dec. 08  *\n";
+	std::cout << "**********************************\n";
+}
+
+void Exer_2_2()
+{
+	// Completed Exercise 2
+	std::cout << "CCCCCCCCC        ++                ++\n";
+	std::cout << "CC*              ++                ++\n";
+	std::cout << "CC*        ++++++++++++++    ++++++++++++++\n";
+	std::cout << "CC*        ++++++++++++++    ++++++++++++++\n";
+	std::cout << "Cc*              ++                ++\n";
+	std::cout << "CCCCCCCCC        ++                ++\n";
+}
+
+void Exer_2_3()
+{
+
+}
+
+void Exer_2_4()
+{
+
+}
+
+void Exer_2_5()
+{
+
+}
+
+void Exer_2_6()
+{
+
+}
+
+void Exer_2_7()
+{
+
+}
+
+void Exer_2_8()
+{
+
+}
+
+void Exer_2_9()
+{
+
+}
+
+void Exer_2_10()
+{
+
+}
+
+void Exer_2_11()
+{
+
+}
+
+void Exer_2_12()
+{
+
+}
+
+void Exer_2_13()
+{
+
+}
+
+void Exer_2_14()
+{
+
+}
+
+void Exer_2_15()
+{
+
+}
+
+void Exer_2_16()
+{
+
+}
+
+void Exer_2_17()
+{
+
+}
+
+void Exer_2_18()
+{
+
+}
+
+void Exer_2_19()
+{
+
+}
+
+void Exer_2_20()
+{
+
+}
+
+void Exer_2_21()
+{
+
+}
+
+void Exer_2_22()
+{
+
+}
+
+void Exer_2_23()
+{
+
+
+}
+
+void Exer_2_24()
+{
+
+}
+
+void Exer_2_25()
+{
+
+}
+
+void Exer_2_26()
+{
+
+}
+
+void Exer_2_27()
+{
+
+
+}
+
+
+*/
